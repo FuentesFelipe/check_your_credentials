4c1df3c581c2d30ed1de8687a8a11a822409b631
FuentesFelipe <felipe.fuentes@u-payments.com>
2021-01-03 16:00:23 -0300

    Descarga opcional

--- encuentra_tu_pass.py
+++ encuentra_tu_pass.py
@@ -3,6 +3,7 @@ import sys, mmap
 import difflib
 import requests
 import os
+from tqdm import tqdm
 
 try: 
     from googlesearch import search 
@@ -18,16 +19,22 @@ def download(url: str, dest_folder: str):
 
     r = requests.get(url, stream=True)
     if r.ok:
-        print("saving to", os.path.abspath(file_path))
+        print(bcolors.WARNING + "saving as " + bcolors.OKBLUE + filename)
+        total_size_in_bytes= int(r.headers.get('content-length', 0))
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+
         with open(file_path, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024 * 8):
+                progress_bar.update(len(chunk))
                 if chunk:
                     f.write(chunk)
                     f.flush()
                     os.fsync(f.fileno())
+        progress_bar.close()
+        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+            print("ERROR, something went wrong")
     else:  # HTTP status code 4XX/5XX
         print("Download failed: status code {}\n{}".format(r.status_code, r.text))
-
 
 class bcolors:
     HEADER = '\033[95m'
@@ -50,41 +57,38 @@ def check_if_string_in_file(file_name, string_to_search):
                 return True
     return False
 
-my_file = open('rockyou.txt', "r", encoding="ISO-8859-1")
-content_list = my_file. readlines()
-''' print("largo: ", len(content_list)) '''
+parser = argparse.ArgumentParser(description='Encuentra tu password en diccionarios públicos')
+parser.add_argument('-p', '--pass', required=True, help=' -p mi_password ')
+parser.add_argument('-d', '--download', action="store_true" ,help=' Usa la flag -d si quieres descargar los wordlists')
+args = vars(parser.parse_args())
 
-if not os.path.exists('Wordlists'):
-    os.makedirs('Wordlists')
+password = args['pass']
+download_param = args['download']
+path_wordlist = "Wordlists"
 
 try:
 
-    parser = argparse.ArgumentParser(description='Encuentra tu password en diccionarios públicos')
-    parser.add_argument('-p', '--pass', required=True, help=' -p mi_password ')
-    parser.add_argument('-d', '--dictio', help=' -d dictionary.txt')
-
-    args = vars(parser.parse_args())
-    password = args['pass']
     query = '"' + str(password) + '"wordlist" "filetype:txt'
-    for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
-        print(j) 
-        if j.find('/'):
-            filename = j.rsplit('/', 1)[1]
-            download(j, dest_folder="Wordlist")
 
-    if args['dictio']:
+    for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
+        print(bcolors.OKGREEN + "URL ->",j) 
+        if download_param:
+                
+            if j.find('/'):
+                filename = j.rsplit('/', 1)[1]
+                download(j, dest_folder=path_wordlist)
     
-        dictionary = args['dictio']
-            
-    else:
-        dictionary= 'rockyou.txt'
+        
+    #my_file = open('rockyou.txt', "r", encoding="ISO-8859-1")
+    #content_list = my_file. readlines()
+    #print("largo: ", len(content_list))
     
-        # Check if string 'is' is found in file 'sample.txt'
-    if check_if_string_in_file('rockyou.txt', password):
-        ''' result = difflib.get_close_matches(password, content_list) '''
-        print('Yes, string found in file. '''' , result ''')
-    else:
-        print('String not found in file')        
+    # Check if string 'is' is found in file 'sample.txt'
+    #if check_if_string_in_file('rockyou.txt', password):
+    #    result = difflib.get_close_matches(password, content_list)
+    #    print('Yes, string found in file. ', result)
+    #else:
+    #    print('String not found in file')        
    
 except:
         
